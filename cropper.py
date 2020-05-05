c13c429921908459ce159ad23bca63e912d0b807
caiosuzuki <cpolidoro@gmail.com>
2020-05-05 01:06:00 -0400

    commenting the viewer and cropper classes to make them clearer, removing main.py that was obsolete

--- cropper.py
+++ cropper.py
@@ -14,9 +14,12 @@ class Cropper():
         self._blocks = blocks
         self._parcelsx = parcelsx
         self._parcelsy = parcelsy
-
+        
+        # X coordinates of the corners of the blocks, list of lists: every list is a list of 4 corners
         self.gridCornersX = []
+        # Y coordinates of the corners of the blocks, list of lists: every list is a list of 4 corners
         self.gridCornersY = []
+        # set of all the points of the grid, list of lists: every list is a block (grid)
         self._grids = []
 
         self._w = self._h = 0
@@ -65,22 +68,29 @@ class Cropper():
     
     def calcGridsPoints(self) -> np.ndarray:
 
+        # for every block...
         for gridCornersX, gridCornersY in \
             zip(self.gridCornersX, self.gridCornersY):
 
             dst = np.array([gridCornersX, gridCornersY], dtype="float32")
             dst = dst.T
+            # calculating the rectangles from the trapeziums through the transform function
             src = self.getDstTransformPoints(dst)
 
+            # Base transform matrix that links rectangles to their respective trapeziums
             M = cv2.getPerspectiveTransform(src, dst)
 
+            # src[1,0] is width of the resulting rectangle (block)
             x_len = src[1,0] / self._parcelsx
+            # src[3,1] is height of the resulting rectangle (block)
             y_len = src[3,1] / self._parcelsy
 
             grid = np.zeros((self._parcelsy+1, self._parcelsx+1, 2))
             for y in range(self._parcelsy+1):
                 for x in range(self._parcelsx+1):
+                    # through the known points of the transformed rectangle, calculates the points in the corresponding rectangle using the M matrix
                     point = M @ np.array([x*x_len, y*y_len, 1], dtype="float32")
+                    # divides by homogeneous coordinate to get the real coordinates
                     grid[y, x] = point[:2] / point[2]
             
             self._grids.append(grid)
@@ -117,50 +127,6 @@ class Cropper():
                 crop[y, x] = np.flip(img[int(i), int(j)])
         
         return crop
-        
-
-    def saveCropsP4(self, img: np.ndarray, 
-                          outputdir: str = "",
-                          timestamp: str = "", 
-                          scale: float = 1) -> None:
-        s = scale
-        for k, g in enumerate(self._grids):
-            for i in range(self._parcelsy):
-                for j in range(self._parcelsx):
-                    dst = np.array([s*g[i,j], s*g[i, j+1],  s*g[i+1, j+1], s*g[i+1, j]],
-                                   dtype="float32")
-                    src = self.getDstTransformPoints(dst)
-                    crop = self.getCrop(img, src, dst)
-                    block, number = self.make_numbers_p4((8*k)+i, j, self._parcelsy, 4)
-                    name = outputdir + timestamp + "-" + "%02d" % (block) + "-" + "%02d" % (number) + ".png"
-                    cv2.imwrite(name, crop)
-    
-    def make_numbers_p4(self, line, column, l, c) -> Tuple[int, int]:
-        blocks = np.array([[64, 65, 66, 67, 68, 69, 70],
-                        [63, 62, 61, 60, 59, 58, 57],
-                        [50, 51, 52, 53, 54, 55, 56],
-                        [49, 48, 47, 46, 45, 44, 43],
-                        [36, 37, 38, 39, 40, 41, 42],
-                        [35, 34, 33, 32, 31, 30, 29],
-                        [22, 23, 24, 25, 26, 27, 28],
-                        [21, 20, 19, 18, 17, 16, 15],
-                        [8, 9, 10, 11, 12, 13, 14],
-                        [7, 6, 5, 4, 3, 2, 1]])
-
-        block1 = np.array([[8, 9, 24, 25],
-                        [7, 10, 23, 26],
-                        [6, 11, 22, 27],
-                        [5, 12, 21, 28],
-                        [4, 13, 20, 29],
-                        [3, 14, 18, 31],
-                        [2, 15, 16, 30],
-                        [1, 16, 17, 32]])
-        block2 = np.flip(block1, axis=1)
-
-        a = line // l
-        blockNumber = blocks[a, column // c]
-        block = block1 if a % 2 == 0 else block2
-        return blockNumber, block[line % l, column % c]
     
     def saveCropsP2(self, img: np.ndarray, 
                           outputdir: str = "",
@@ -170,14 +136,18 @@ class Cropper():
         for k, g in enumerate(self._grids):
             for i in range(self._parcelsy):
                 for j in range(self._parcelsx):
+                    # scales every point of the block
                     dst = np.array([s*g[i,j], s*g[i, j+1],  s*g[i+1, j+1], s*g[i+1, j]],
                                    dtype="float32")
+                    # gets the corresponding rectangle of the (scaled) trapezium
                     src = self.getDstTransformPoints(dst)
+                    # given the coordinates, extracts the crop of the parcel from the original image
                     crop = self.getCrop(img, src, dst)
                     block, number = self.make_numbers_p2(i, j, self._parcelsy, self._parcelsx, k+1)
                     name = outputdir + timestamp + "-" + "%02d" % (block) + "-" + "%03d" % (number) + ".png"
                     cv2.imwrite(name, crop)
     
+    # this is the implementation to track the zigzag numbers of the parcels in the P2 Croqui, check the Croqui to understand this better
     def make_numbers_p2(self, line, col, numLines, numColumns, p2Block: int):
         if col % 2 == 0:
             oy = numLines - 1
--- viewer.py
+++ viewer.py
@@ -10,15 +10,17 @@ from cropper import Cropper
 
 def makeParser() -> ArgumentParser:
     parser = ArgumentParser()
-    parser.add_argument("img", type=str, help="Image path")
-    parser.add_argument("--blocks", type=int, default=1, help="number of blocks")
+    parser.add_argument("img", type=str, help="Image path (.tif file)")
+    parser.add_argument("--blocks", type=int, default=1, help="number of blocks (sets of several parcels, \
+        every block has to have the same amount of parcels)")
     parser.add_argument("--grid_x", type=int, default=1, help="number  of \
                         parcels inside each block in x axis")
     parser.add_argument("--grid_y", type=int, default=1, help="number  of \
                         parcels inside each block in y axis")
-    parser.add_argument("--points", type=str, help="file with the 4 points of the block")
-    parser.add_argument("--save_points", action="store_true")
-    parser.add_argument("--timestamp", type=str, default="0000-00-00")
+    parser.add_argument("--points", type=str, help="file with the 4 points of the block, used to avoid using the GUI everytime")
+    parser.add_argument("--save_points", help="saves the user's drawn points in a file named <tif_file_name>.npy", action="store_true")
+    parser.add_argument("--timestamp", help="timestamp used to name the generated images' names", type=str, default="0000-00-00")
+    # the filenames are going to be: YYYY-MM-DD-BLOCKID-PARCELID
     parser.add_argument("--output_dir", type=str, default="", help="output directory\
                             where files should be saved")
 
@@ -31,6 +33,7 @@ def calc_overviews(dataset) -> None:
     
     if not have_overviews:
         print("Calculating Overviews")
+        # hard-coded zoom levels in the ortomosaic
         factors = [1, 4, 8, 16]
         dataset.build_overviews(factors, Resampling.average)
         dataset.update_tags(ns="rio_overview", resampling="average")
@@ -60,15 +63,20 @@ if __name__ == '__main__':
         print("Transform:")
         print(dataset.transform)
         calc_overviews(dataset)
+        # o: original
+        # d: redimensioned (1/4 of original img size)
         ro, go, bo = dataset.read([1,2,3], out_shape=(3, dataset.height, dataset.width))
         rd, gd, bd = dataset.read([1,2,3], out_shape=(3, dataset.height // 4, dataset.width // 4))
+        # rgb representation of the img
         rgb = np.dstack((rd, gd, bd))
         print("Image loaded")
     
     
     if args.points is not None:
+        # pre-loaded points
         mycropper = Cropper(args.blocks, args.grid_x, args.grid_y, np.load(args.points))
     else:
+        # user has to draw the points
         mycropper = Cropper(args.blocks, args.grid_x, args.grid_y)
     
     
@@ -79,14 +87,18 @@ if __name__ == '__main__':
        gui.pause()
     
     mycropper.sortCorners()
-    # gui.drawRectangle(mycropper.gridCornersX, mycropper.gridCornersY)
+    gui.drawRectangle(mycropper.gridCornersX, mycropper.gridCornersY)
     grids = mycropper.calcGridsPoints()
     for points in grids:
         gui.drawPoints(points[..., 0], points[..., 1])
     
     gui.drawGridsLines(grids, color='r')
 
+    # tip: comment the if below if you only want to view if your drawing of the blocks was good enough to generate decent parcels
     if args.output_dir != "":
+        # scale must be the same rate of the redimension of rd, gd, bd
+        # the function used below will be changed for the corresponding experiment that you implement
+        # TODO: turn this into a parsed argument
         mycropper.saveCropsP2(np.dstack((ro, go, bo)), 
                             outputdir=args.output_dir,
                             timestamp=args.timestamp,
