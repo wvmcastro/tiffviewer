c2bbd6ac200b4a5a4f9a60f2b42a393c532bf4b6
luizhss <54457561+luizhss@users.noreply.github.com>
2020-05-27 12:50:02 -0400

    Update make_numbers_triangulo_p7.py

--- make_numbers_triangulo_p7.py
+++ make_numbers_triangulo_p7.py
@@ -1,1 +1,4 @@
 # implement make_numbers_triangulo_p7 here
+def make_numbers_triangulo_p7(self, line, col, numLines, numColumns, p2Block: int):
+    index = numColumns*line + numColumns-col
+    return p2Block, index
